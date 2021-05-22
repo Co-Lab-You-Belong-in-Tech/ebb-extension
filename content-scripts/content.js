a3ce129867a240123802c1f7bb4a5c5aaad24000
Franco Ortega <francopdx@gmail.com>
2021-05-21 20:00:04 -0700

    add onclick to popup button to activate extension

--- content-scripts/content.js
+++ content-scripts/content.js
@@ -2,7 +2,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     $("head").prepend(
         `<style>
             img[src*=".gif"] {
-                opacity: 75%
+                opacity: 0%
             }
             img[src*=".GIF"] {
                 opacity: 75%
--- popup/popup.js
+++ popup/popup.js
@@ -1,2 +1,20 @@
-// JS file for popup.html
 let stopGif = document.getElementById("stopGif");
+
+if(stopGif) {
+    stopGif.onclick = function() {
+        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+            chrome.tabs.sendMessage(
+                tabs[0].id,
+                {
+                    imageDivId: `${guidGenerator()}`
+                }
+            );
+            function guidGenerator() {
+                const S4 = function() {
+                    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+                };
+                return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+            }
+        });
+    };
+}
