c7d23516120e3660bedc330ccb4f7a3ba8e0644b
Franco Ortega <francopdx@gmail.com>
2021-05-21 17:17:16 -0700

    reduce the opacity of GIFs with the .gif and .GIF file name

--- content-scripts/content.js
+++ content-scripts/content.js
@@ -1,1 +1,13 @@
-// JS folder for extension functionality
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    $("head").prepend(
+        `<style>
+            img[src*=".gif"] {
+                opacity: 75%
+            }
+            img[src*=".GIF"] {
+                opacity: 75%
+            }
+        </style>`
+    );
+    sendResponse({ fromcontent: "This message is from content.js" });
+});
