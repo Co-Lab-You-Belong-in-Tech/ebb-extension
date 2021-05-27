ae5ba3f37eac46f69e7895b18fa21df4065c8f07
Franco Ortega <francopdx@gmail.com>
2021-05-27 09:26:52 -0700

    target all tabs on all windows that have completed loading

--- popup/popup.js
+++ popup/popup.js
@@ -2,22 +2,27 @@ let stopGif = document.getElementById("stopGif");
 
 if(stopGif) {
     stopGif.onclick = function() {
-        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-            chrome.tabs.sendMessage(
-                tabs[0].id,
-                {
-                    imageDivId: `${guidGenerator()}`
-                },
-                function(response) {
-                    window.close();
-                }
-            );
-            function guidGenerator() {
-                const S4 = function() {
-                    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-                };
-                return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
-            }
+        chrome.tabs.query(
+            { status: "complete" },
+            function(tabs) {
+                tabs.forEach(function(tab){
+                    chrome.tabs.sendMessage(
+                        tab.id,
+                        {
+                            imageDivId: `${guidGenerator()}`,
+                            tabId: tab.id
+                        },
+                        function(response) {
+                            window.close();
+                        }
+                    );
+                    function guidGenerator() {
+                        const S4 = function() {
+                            return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+                        };
+                        return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+                    }
+                })
         });
     };
 }
