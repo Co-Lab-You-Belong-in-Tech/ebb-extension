02835fda3ba1da0dc2a352db6cdaa74b2767da32
tron-ii <frank.ihekwaba@gmail.com>
2021-05-28 11:04:59 -0400

    refactored code

--- content-scripts/content.js
+++ content-scripts/content.js
@@ -1,3 +1,4 @@
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     $("head").prepend(
         `<style>
--- popup/popup.js
+++ popup/popup.js
@@ -1,28 +1,31 @@
-let stopGif = document.getElementById("stopGif");
+//let stopGif = document.getElementById("stopGif");
 
-if(stopGif) {
-    stopGif.onclick = function() {
-        chrome.tabs.query(
-            { status: "complete" },
-            function(tabs) {
-                tabs.forEach(function(tab){
-                    chrome.tabs.sendMessage(
-                        tab.id,
-                        {
-                            imageDivId: `${guidGenerator()}`,
-                            tabId: tab.id
-                        },
-                        function(response) {
-                            window.close();
-                        }
-                    );
-                    function guidGenerator() {
-                        const S4 = function() {
-                            return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-                        };
-                        return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+function allTabs() {
+    chrome.tabs.query(
+        { status: "complete" },
+        function(tabs) {
+            tabs.forEach(function(tab){
+                chrome.tabs.sendMessage(
+                    tab.id,
+                    {
+                        imageDivId: `${guidGenerator()}`,
+                        tabId: tab.id
+                    },
+                    function(response) {
+                        window.close();
                     }
-                })
-        });
-    };
+                );
+                function guidGenerator() {
+                    const S4 = function() {
+                        return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+                    };
+                    return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+                }
+            })
+    });
 }
+document.getElementById("vanishGif").addEventListener("click", allTabs);
+
+
+
+ 
