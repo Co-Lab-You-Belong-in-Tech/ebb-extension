74096896a99d25badfc064ad91503b4a2e946fe2
Franco Ortega <francopdx@gmail.com>
2021-05-28 19:02:28 -0700

    remove unnecessary guidGenerator method

--- popup/popup.js
+++ popup/popup.js
@@ -9,20 +9,14 @@ if(stopGif) {
                     chrome.tabs.sendMessage(
                         tab.id,
                         {
-                            imageDivId: `${guidGenerator()}`,
                             tabId: tab.id
                         },
                         function(response) {
                             window.close();
                         }
                     );
-                    function guidGenerator() {
-                        const S4 = function() {
-                            return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-                        };
-                        return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
-                    }
                 })
-        });
+            }
+        );
     };
 }
